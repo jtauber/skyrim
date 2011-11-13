fca6bbb254d6cb1b07e9e6a8d4494c18157ae1b9
James Tauber <jtauber@jtauber.com>
2011-11-13 06:20:14 -0500

    tes reader properly reads record size now and just reads top level groups

--- tes.py
+++ tes.py
@@ -16,22 +16,34 @@ else:
 f = open(TES_FILE)
 
 while True:
-    block_type = struct.unpack("<4s", f.read(4))[0]
+    data = f.read(4)
+    if not data:
+        break
+    block_type = struct.unpack("<4s", data)[0]
     print block_type,
-    if block_type in ["TES4", "GRUP", "GMST", "KYWD", "LCRT", "AACT", "TXST",
-            "GLOB", "CLAS", "MGEF"]:
-        # these blocks seem to be a fixed size rather than using the first
-        # two bytes to indicate length
-        print "-"
-        print "\t%08X%08X%08X%08X%08X" % struct.unpack("<LLLLL", f.read(20))
-    elif block_type in ["HEDR", "CNAM", "INTV", "EDID", "DATA", "OBND",
-            "TX00", "TX01", "DNAM", "DODT", "TX03", "TX05", "TX07", "TX02",
-            "TX04", "FNAM", "FLTV", "FULL", "DESC", "FACT", "VENV", "PLVD",
-            "MAST", "MDOB", "KSIZ", "KWDA"]:
-        # these blocks seem to indicate their length in the first two bytes
-        length = struct.unpack("<H", f.read(2))[0]
+    if block_type in ["GRUP"]:
+        length = struct.unpack("<L", f.read(4))[0]
         print length
-        f.read(length)
+        f.read(16)
+    elif block_type in ["TES4", "GMST", "KYWD", "LCRT", "AACT", "TXST",
+            "GLOB", "CLAS", "FACT", "HDPT", "EYES", "RACE", "SOUN", "ASPC",
+            "MGEF", "LTEX", "ENCH", "SPEL", "SCRL", "ACTI", "TACT", "ARMO",
+            "BOOK", "CONT", "DOOR", "INGR", "LIGH", "MISC", "APPA", "STAT",
+            "MSTT", "GRAS", "TREE", "FLOR", "FURN", "WEAP", "AMMO", "NPC_",
+            "LVLN", "KEYM", "ALCH", "IDLM", "COBJ", "PROJ", "HAZD", "SLGM",
+            "LVLI", "WTHR", "CLMT", "SPGD", "RFCT", "REGN", "NAVI", "CELL",
+            "REFR", "ACHR", "PGRE", "NAVM", "PHZD", "WRLD", "LAND", "DIAL",
+            "INFO", "QUST", "IDLE", "PACK", "CSTY", "LSCR", "LVSP", "ANIO",
+            "WATR", "EFSH", "EXPL", "DEBR", "IMGS", "IMAD", "FLST", "PERK",
+            "BPTD", "ADDN", "AVIF", "CAMS", "CPTH", "VTYP", "MATT", "IPCT",
+            "IPDS", "ARMA", "ECZN", "LCTN", "MESG", "DOBJ", "LGTM", "MUSC",
+            "FSTP", "FSTS", "SMBN", "SMQN", "SMEN", "DLBR", "MUST", "DLVW",
+            "WOOP", "SHOU", "EQUP", "RELA", "SCEN", "ASTP", "OTFT", "ARTO",
+            "MATO", "MOVT", "SNDR", "DUAL", "SNCT", "SOPM", "COLL", "CLFM",
+            "REVB", "NAVI"]:
+        length = struct.unpack("<L", f.read(4))[0]
+        print length
+        f.read(length + 16)
     else:
         print "unknown"
         sys.exit(1)
